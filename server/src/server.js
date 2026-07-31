f93c2c86da0170c42be1a0f44debef8074ddeee6
Aryan Kondhalkar <aryankondhalkar13@gmail.com>
2026-07-31 13:18:10 +0530

    Connect MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,8 @@
-import 'dotenv/config'
-import app from './app.js'
+import "dotenv/config";
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+await connectDB();
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
