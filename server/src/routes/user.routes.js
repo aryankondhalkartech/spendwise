d2c93cb7e2a6c39efc7d9a0ba8f912143fc9c3b4
Aryan Kondhalkar <aryankondhalkar13@gmail.com>
2026-08-01 18:16:34 +0530

    Fix request validation middleware

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.patch(
   "/preferences",
-  validateRequest(updateCurrencyPreference),
+  validateRequest(updateCurrencyPreferenceSchema),
   updateCurrencyPreference,
 );
 
