d187f0f78dbd574692fd14b96f85b03c39de42ad
Aryan Kondhalkar <aryankondhalkar13@gmail.com>
2026-08-01 18:09:44 +0530

    Fix ObjectId validation middleware

--- server/src/middlewares/validate-object-id.middleware.js
+++ server/src/middlewares/validate-object-id.middleware.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const validateObjectId = (req, res, next) => {
-  if (!mongoose.Schema.Types.ObjectId.isValid(req.params.id)) {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.status(400).json({
       success: false,
       message: "Invalid resource id",
